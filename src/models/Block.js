5a5238fa80cd1a4516108a9cf7fed41509f6f646
1303-yzym <118655639+1303-yzym@users.noreply.github.com>
2023-05-24 21:00:59 +0800

    Update Block.js
    aa

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,9 +1,17 @@
 class Block {
-  // 1. 完成构造函数及其参数
-  /* 构造函数需要包含
+    // 1. 完成构造函数及其参数
+    /* 构造函数需要包含
 
-  */
-  constructor() {}
+    */
+    constructor(blockChain, previousHash,index,hash) {      
+        this.blockChain = blockChain;
+        this.previousHash = previousHash;
+        this.index=index
+        this.hash=hash.toString()
+        Block.prototype.hash=hash.toString()
+        blockChain.addBlock(this)
+    }
+
 }
 
 export default Block
